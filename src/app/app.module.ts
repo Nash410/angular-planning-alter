db939da148254a103679ad7e033418593f652698
Nash410 <christophe.jacquenet@gmail.com>
2019-11-26 10:11:56 +0100

    1 file modified, 15 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AuthService } from './service/auth.service';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AbsComponent } from './abs/abs.component';
+import { AuthGuard } from './service/AuthGuard';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, SignupComponent, SigninComponent, AbsComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [AuthService, AuthGuard]
 })
 export class AppModule { }
