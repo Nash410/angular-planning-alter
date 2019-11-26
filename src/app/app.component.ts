60187c0369398b3eb06194e57d7492748472485e
Nash410 <christophe.jacquenet@gmail.com>
2019-11-26 12:02:46 +0100

    8 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'my-app',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyAA-9XbgYNft6uYdMB71veOfebyuI6BNaA",
+      authDomain: "planningalter-34ec0.firebaseapp.com",
+      databaseURL: "https://planningalter-34ec0.firebaseio.com",
+      projectId: "planningalter-34ec0",
+      storageBucket: "planningalter-34ec0.appspot.com",
+      messagingSenderId: "312869238770",
+      appId: "1:312869238770:web:18879e5eee49de43049d6b"
+    };
+    firebase.initializeApp(config);
+  }
 }
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
 })
-export class SigninComponent implements OnInit {
+export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signinForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
+
+  onSubmit() {
+    const email = this.signinForm.get('email').value;
+    const password = this.signinForm.get('password').value;
+    
+    this.authService.signInUser(email, password).then(
+      () => {
+        this.router.navigate(['/books']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
