d58a64675392b125d1bbcb12eb73bcc2125ca290
Nash410 <christophe.jacquenet@gmail.com>
2019-11-26 10:16:10 +0100

    6 files added

---
+++ src/app/alter/form-alter/form-alter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-alter',
+  templateUrl: './form-alter.component.html',
+  styleUrls: ['./form-alter.component.css']
+})
+export class FormAlterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/alter/single-alter/single-alter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-alter',
+  templateUrl: './single-alter.component.html',
+  styleUrls: ['./single-alter.component.css']
+})
+export class SingleAlterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
