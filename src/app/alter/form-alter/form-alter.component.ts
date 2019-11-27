51027546da52b47250af24f1a1be3339d1e3aa66
Nash410 <christophe.jacquenet@gmail.com>
2019-11-27 14:53:02 +0100

    4 files modified

--- src/app/alter/form-alter/form-alter.component.ts
+++ src/app/alter/form-alter/form-alter.component.ts
@@ -12,6 +12,8 @@ import { Router } from '@angular/router';
 export class FormAlterComponent implements OnInit {
 
 alterForm: FormGroup;
+errorMessage: string;
+
 
 constructor(private formBuilder: FormBuilder,
               private alterService: AlterService,
@@ -23,32 +25,19 @@ constructor(private formBuilder: FormBuilder,
 
   initForm() {
     this.alterForm = this.formBuilder.group({
-      nom_alternant : ['', [Validators.required, Validators.email]],
+      nom_alternant : ['', [Validators.required]],
       dateD : ['', [Validators.required]],
       dateF : ['', [Validators.required]]
 
-    }, {validator: this.dateLessThan('dateD', 'dateF')});
+    });
   }
 
-  dateLessThan(from: string, to: string) {
-    return (group: FormGroup): {[key: string]: any} => {
-      let f = group.controls[from];
-      let t = group.controls[to];
-      if (f.value > t.value) {
-        return {
-          dates: "Période de contrat incorrecte."
-        };
-      }
-      return {};
-    }
-}
-
-  onSave() {
+  onSaveAlter() {
     const nom_alternant = this.alterForm.get('nom_alternant').value;
     const dateD = this.alterForm.get('dateD').value;
     const dateF = this.alterForm.get('dateF').value;
     const newAlter = new AlterModel(nom_alternant, dateD, dateF);
-    this.alterService.createNewAlter(newAlter)
+    this.alterService.createNewAlter(newAlter);
     this.router.navigate(['/alter']);
 
   }
--- src/app/model/alter.model.ts
+++ src/app/model/alter.model.ts
@@ -1,8 +1,12 @@
 export class AlterModel {
   private nomAlter:string;
-  private dateDebutContrat:Date;
-  private dateFinContrat:Date;
+  private dateDebutContrat:string;
+  private dateFinContrat:string;
   private pa_user?:number;
-  constructor(nomAlter:string, dateDebutContrat:Date, dateFinContrat:Date, pa_user?:number){
+  constructor(nomAlter:string, dateDebutContrat:string, dateFinContrat:string, pa_user?:number){
+    this.nomAlter = nomAlter;
+    this.dateDebutContrat = dateDebutContrat;
+    this.dateFinContrat = dateFinContrat;
+    //this.pa_user = pa_user;
   }
 }
