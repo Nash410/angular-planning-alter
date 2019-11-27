9566217aaca31a61a9df2b321b3c7ee8f7ffbf57
Nash410 <christophe.jacquenet@gmail.com>
2019-11-27 11:29:44 +0100

    3 files modified

--- src/app/alter/alter.component.ts
+++ src/app/alter/alter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlterService } from '../service/alter.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alter',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlterComponent implements OnInit {
 
-  constructor() { }
+  alterForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private alterService: AlterService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.alterForm = this.formBuilder.group({
+      nom_alternant : ['', [Validators.required, Validators.email]],
+      dateD : ['', [Validators.required]],
+      dateF : ['', [Validators.required]]
+
+    }, {validator: this.dateLessThan('dateD', 'dateF')});
+  }
+
+  dateLessThan(from: string, to: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let f = group.controls[from];
+      let t = group.controls[to];
+      if (f.value > t.value) {
+        return {
+          dates: "Période de contrat incorrecte."
+        };
+      }
+      return {};
+    }
+}
+
+  onSubmit() {
+    const nom_alternant = this.alterForm.get('nom_alternant').value;
+    const dateD = this.alterForm.get('dateD').value;
+    const dateF = this.alterForm.get('dateF').value;
+
+    this.alterService.createNewAlter(nom_alternant, dateD, dateF).then(
+      () => {
+        this.router.navigate(['/alter']);
+      },
+      (error) => {
+        this.errorMessage = error;
+      }
+    );
+  }
 }
--- src/app/service/alter.service.ts
+++ src/app/service/alter.service.ts
@@ -1,8 +1,70 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { AlterModel } from '../model/alter-model';
+import * as firebase from 'firebase';
+
 
 @Injectable()
 export class AlterService {
 
-  constructor() { }
+  alters: AlterModel[] = [];
+  altersSubject = new Subject<AlterModel[]>();
+
+  emitAlter() {
+    this.altersSubject.next(this.alters);
+  }
+
+  saveAlter(){
+    firebase.database().ref('/alter').set(this.alters);
+  }
+
+  getAlters(){
+    firebase.database().ref('/alter')
+      .on('value', (data: firebase.database.DataSnapshot)=>{
+        this.alters = data.val() ? data.val() : [];
+        this.emitAlter();
+      })
+  }
+
+  getSingleAlter(id:number){
+    return new Promise(
+      (resolve, reject)=>{
+        firebase.database().ref('/alter/' + id).once('value').then(
+          (data: firebase.database.DataSnapshot)=> {
+            resolve(data.val());
+          }, (error) =>{
+            reject(error);
+          }
+        )
+      }
+    )
+  }
+
+  createNewAlter(newAlter: AlterModel){
+    this.alters.push(newAlter);
+    this.saveAlter();
+    this.emitAlter();
+  }
+
+  removeAlter(alter: AlterModel){
+    const alterIndexToRemove = this.alters.findIndex(
+      (alterEl)=>{
+        if (alterEl === alter ){
+          return true
+        }
+      }
+    );
+    this.alters.splice(alterIndexToRemove, 1);
+    this.saveAlter();
+    this.emitAlter();
+  }
+
+  constructor(){
+    this.getAlters();
+  }
+
+
+
 
 }
+
