636f0bd09aa3ec2be496d80410150c6f04809cd1
Nash410 <christophe.jacquenet@gmail.com>
2019-11-29 12:01:35 +0100

    2 files modified, 1 added

--- src/app/planning/planning.component.ts
+++ src/app/planning/planning.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanningComponent implements OnInit {
 
-  constructor() { }
+  today:Date;
+  years: number[];
+  months: string[];
+  week: string[];
+
+  constructor() {}
 
   ngOnInit() {
+    this.today = new Date();
+    this.months = new Array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre");
+    this.week  = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"); 
+    let currentYear = this.today.getFullYear();
+    this.years = new Array(currentYear-1, currentYear, currentYear+1);
   }
+
+
+
 
 }
